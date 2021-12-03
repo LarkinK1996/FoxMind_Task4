32577a66621a5c2d5e86f541fb89affbee8fc968
Konstantin <larkin.k.1996@mail.ru>
2021-12-03 16:50:36 +0300

    add const

--- src/main/java/com/foxminded/int_division/Application.java
+++ src/main/java/com/foxminded/int_division/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         Division myProgram = new Division();
         int dividend = 78945;
-        int divider = 40;
+        int divider = 4;
         ArrayList<Step> steps = myProgram.fillTheListSteps(dividend, divider);
 
         DivisionVisualization divisionVisualization = new DivisionVisualization();
--- src/main/java/com/foxminded/int_division/DivisionVisualization.java
+++ src/main/java/com/foxminded/int_division/DivisionVisualization.java
@@ -3,25 +3,26 @@ package com.foxminded.int_division;
 import java.util.ArrayList;
 
 public class DivisionVisualization {
+    private static final String space = " ";
+    private static final String underline = "_";
 
-    void visualization(ArrayList<Step> steps, int dividend) {
+    public void visualization(ArrayList<Step> steps, int dividend) {
         int remainder = 0;
         System.out.println(dividend + "|" + steps.get(0).divider);
-        System.out.println("_");
+        System.out.println(underline);
         StringBuffer spaces = new StringBuffer();
 
         for (Step step : steps) {
-
             int dividendDigit = getCountsOfDigits(step.dividend);
             int dividerDigit = getCountsOfDigits(step.divider);
             StringBuffer underlining = new StringBuffer();
-            spaces.append(" ");
-            for(int i=0;i<dividendDigit;i++){
-                underlining.append("_");
+            spaces.append(space);
+            for (int i = 0; i < dividendDigit; i++) {
+                underlining.append(underline);
             }
 
             System.out.println(spaces.toString() + step.dividend);
-            System.out.println("_");
+            System.out.println(underline);
             System.out.println(spaces.toString() + step.localDividend + "   ");
             System.out.println(spaces.toString() + underlining);
             remainder = step.remainder;
