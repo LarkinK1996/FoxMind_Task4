6e9c2e8437faede9d1f2088d12cf9ba0601e549c
Konstantin <larkin.k.1996@mail.ru>
2021-12-12 17:46:52 +0300

    delete unnecessary dependencies, add body visualization

--- src/main/java/com/foxminded/division/model/DivisionResult.java
+++ src/main/java/com/foxminded/division/model/DivisionResult.java
@@ -8,22 +8,6 @@ public class DivisionResult {
     private int quotient;
     private ArrayList<DivisionStep> divisionSteps;
 
-    public void setDividend(int dividend) {
-        this.dividend = dividend;
-    }
-
-    public void setDivisor(int divisor) {
-        this.divisor = divisor;
-    }
-
-    public void setQuotient(int quotient) {
-        this.quotient = quotient;
-    }
-
-    public void setSteps(ArrayList<DivisionStep> divisionSteps) {
-        this.divisionSteps = divisionSteps;
-    }
-
     public int getDividend() {
         return dividend;
     }
