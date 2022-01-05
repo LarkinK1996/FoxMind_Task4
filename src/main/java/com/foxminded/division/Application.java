457c2eee9ff4579f6bed3880ee6b2bd173efaabe
Konstantin <larkin.k.1996@mail.ru>
2022-01-05 14:31:49 +0300

    add tests

--- src/main/java/com/foxminded/division/Application.java
+++ src/main/java/com/foxminded/division/Application.java
@@ -6,8 +6,8 @@ import com.foxminded.division.model.DivisionResult;
 
 public class Application {
     public static void main(String[] args) {
-        int dividend = 122;
-        int divisor = 11;
+        int dividend = 100;
+        int divisor = 123;
         IntegerDivisionCalculator calculator = new IntegerDivisionCalculator();
         IntegerDivisionFormatter formatter = new IntegerDivisionFormatter();
         DivisionResult result = calculator.calculateDivisionResult(dividend, divisor);
